591fa39359f5ce04f06451c21c89d8756386362a
Tropxz/Jimmy <130433432+Tropxzz@users.noreply.github.com>
2024-05-25 18:03:39 +1000

    Update checker.lua

--- checker.lua
+++ checker.lua
@@ -1,7 +1,7 @@
 local placeid = game.PlaceId 
 
-if placeid == 21391283098123 then -- placement since theres only universal out rn
-
+if placeid == 13864667823  then -- placement since theres only universal out rn
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/Games/Breakin2.lua",true))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/universal.lua",true))()
 end
