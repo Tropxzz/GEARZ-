a62339fb0464588a323e7e7a534eda08b900bc97
Tropxz/Jimmy <130433432+Tropxzz@users.noreply.github.com>
2024-05-25 17:23:12 +1000

    Update checker.lua

--- checker.lua
+++ checker.lua
@@ -3,5 +3,5 @@ local placeid = game.PlaceId
 if placeid == 21391283098123 then -- placement since theres only universal out rn
 
 else
-    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/universal.lua",true))()
 end
