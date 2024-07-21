4489a61c604ea2900804ce4d4037940d6b81a9ea
Tropxz/Jimmy <130433432+Tropxzz@users.noreply.github.com>
2024-07-21 14:46:10 +1000

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,6 +1,6 @@
 for i,v in ipairs(game.CoreGui:GetDescendants()) do
     if v:IsA("TextLabel") then
-        if string.find(v.Text, "GEARZ") then
+        if string.find(v.Text, "GEARZ has loaded") then
             v.TextColor3 = Color3.fromRGB(0, 0, 255)
         end
     end
