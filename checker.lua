7aa7a024a5381aad868d2d9ae2030ffb15d76220
Tropxz/Jimmy <130433432+Tropxzz@users.noreply.github.com>
2024-07-21 15:04:27 +1000

    Update checker.lua

--- checker.lua
+++ checker.lua
@@ -2,10 +2,15 @@ local placeid = game.PlaceId
 
 if placeid == 13864667823  then 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/Games/Breakin2.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/a.lua",true))()
 elseif placeid == 16389398622 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/Games/A_DUSTY_TRIP_FOR_NOW",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/a.lua",true))()
 elseif placeid == 15751118261 then
 loadstring(game:HttpGet("https://github.com/Tropxzz/GEARZ-/blob/main/Games/asjdksajdlasjdlakdjalkdjasldjaldjlasjdlasjdlas.lua?raw=true",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/a.lua",true))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/universal.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/a.lua",true))()
+
 end
