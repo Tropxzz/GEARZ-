6987caac260daaa481b5cfdc9dc2fad3b8ddf5f5
Tropxz/Jimmy <130433432+Tropxzz@users.noreply.github.com>
2024-07-23 16:39:57 +1000

    Update checkercode.lua

--- checkercode.lua
+++ checkercode.lua
@@ -9,6 +9,9 @@ elseif placeid == 16389398622 then
 elseif placeid == 15751118261 then
 loadstring(game:HttpGet("https://github.com/Tropxzz/GEARZ-/blob/main/Games/asjdksajdlasjdlakdjalkdjasldjaldjlasjdlasjdlas.lua?raw=true",true))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/a.lua",true))()
+elseif placeid == 648362523 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/Games/ilovebreakingthepointinbreakingpoint.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/a.lua",true))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/universal.lua",true))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/GEARZ-/main/a.lua",true))()
